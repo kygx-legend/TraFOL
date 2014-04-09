dc52b6ee0c7e75c0f8037e7d11b4b2f748f2e9e1
legendlee1314 <legendlee1314@gmail.com>
2014-04-09 13:53:43 +0800

    version 0.0.2

--- structures.py
+++ structures.py
@@ -1,86 +1,226 @@
 """
-structures.py--AST and needed structures.
+structures.py -- AST and neccesary structures.
 Formalization of Programs in First-Order Logic with Discrete Linear Order.
 
-version: 0.0.1
+version: 0.0.2
 author: legendlee(legendlee1314@gmail.com)
 """
 
-# AST(abstract syntax tree).
+# Strutures for AST(abstract syntax tree).
 """
-P -> A                           unary
-P -> P | P                       binary
-P -> B(if) ^ P(then) | P(then)   judge
-P -> B(while) ^ P(do)            loop
+Generating Rules(Double Sides):
+P <-> E                           unary
+P <-> P | P                       binary
+P <-> if(B) ^ then(P1) else(P2)   judge
+P <-> while(B) ^ do(P)            loop
 """
 
-# Axiom Exp Node.
-class AENode:
+# Atomic Expression.
+class AtomicExpression:
     """ 
         p1 = p3
-        p1 = p3 +/- p5
+        p1 = p3 p4(+/)- p5
     """
     def __init__(self, p1, p3, p4=None, p5=None):
-        if p4 is None:
-            self.axiom = (p1, p3)
+        self.atomic = (p1, p3, p4, p5)
+        if p4 is None or p5 is None:
             self.op = False
         else:
-            self.axiom = (p1, p3, p4, p5)
             self.op = True
 
     def show(self):
         if self.op is True:
-            print "AENode: %s = %s %s %s" % self.axiom
+            print "AE: %s = %s %s %s" % self.atomic
         else:
-            print "AENode: %s = %s" % self.axiom
+            print "AE: %s = %s" % self.atomic[0:2]
 
-# Axiom Bool Node.
-class ABNode:
+# Atomic Boolean.
+class AtomicBoolean:
     """ 
-        p1 <,<=,==,>=,> p3
+        p1 p2(<,<=,==,>=,>) p3
     """
     def __init__(self, p1, p2, p3):
-        self.axiom = (p1, p2, p3)
+        self.atomic = (p1, p2, p3)
 
     def show(self):
-        print "ABNode: %s %s %s" % self.axiom
+        print "AB: %s %s %s" % self.atomic
+
+# Expressions Node.
+class ExprNode:
+    def __init__(self, form, next_node):
+        self.form = form
+        self.next_node = next_node 
+        self.pre_node = None
+
+    def set_pre_node(self, pre_node):
+        self.pre_node = pre_node
+
+    def show(self):
+        print "ExprNode:" + self.form
+        if self.form == 'unary':
+            self.next_node['A'].show()
+        elif self.form == 'binary':
+            self.next_node['E1'].show()
+            self.next_node['E2'].show()
 
 # Program Node.
-class PNode:
-    def __init__(self, form, child):
+class ProgNode:
+    def __init__(self, form, next_node):
         self.form = form
+        self.next_node = next_node
+        self.pre_node = None
+
+    def set_pre_node(self, pre_node):
+        self.pre_node = pre_node
+
+    def show(self):
+        print "ProgNode:" + self.form
+        if self.form == 'unary':
+            self.next_node['E'].show()
+        elif self.form == 'binary':
+            self.next_node['P1'].show()
+            self.next_node['P2'].show()
+        elif self.form == 'judge':
+            self.next_node['B'].show()
+            self.next_node['P1'].show()
+            self.next_node['P2'].show()
+        elif self.form == 'loop':
+            self.next_node['B'].show()
+            self.next_node['P'].show()
+
+# Strutures for Intermediate Layer.
+class MidNode:
+    def __init__(self):
+        self.next_node = None
+
+    def set_next_node(self, node):
+        self.next_node = node
+
+class MidExprList(MidNode):
+    def __init__(self):
+        MidNode.__init__(self)
+        self.exprs = []
+
+    def add_expr(self, expr):
+        self.exprs.append(expr)
+
+    def show(self):
+        print "MidExprList"
+
+class MidJudgeNode(MidNode):
+    def __init__(self):
+        MidNode.__init__(self)
+        self.condition = None
+        self.first_child = None
+        self.second_child = None
+
+    def add_condition(self, condition):
+        self.condition = condition
+
+    def set_childs(self, child1, child2):
+        self.first_child = child1
+        self.second_child = child2
+
+    def show(self):
+        print "MidJudgeNode"
+
+class MidLoopNode(MidNode):
+    def __init__(self):
+        MidNode.__init__(self)
+        self.condition = None
+        self.child = None
+
+    def add_condition(self, condition):
+        self.condition = condition
+
+    def set_child(self, child):
         self.child = child
 
     def show(self):
-        print "PNode:"
-        if self.form == 'unary':
-            self.child['A'].show()
-        elif self.form == 'binary':
-            self.child['P1'].show()
-            self.child['P2'].show()
-        elif self.form == 'judge':
-            self.child['B'].show()
-            self.child['P1'].show()
-            self.child['P2'].show()
-        elif self.form == 'loop':
-            self.child['B'].show()
-            self.child['P'].show()
+        print "MidLoopNode"
+
+# Axiom in First-Order Logic.
+class Axiom:
+    def __init__(self, variable, expr, condition=None):
+        self.variable = variable
+        self.expr = expr
+        self.condition = condition
+
+    def set_condition(self, condition):
+        self.condition = condition
+
+    def phi_n(self, variables, natural):
+        self.variable = self.variable + "(%s+1)" % natural
+        for key in variables:
+            self.expr = self.expr.replace(key, "%s(%s)" % (key, natural))
+
+    def show(self):
+        if self.condition is not None:
+            print "%s -> %s = %s" % (self.condition, self.variable, self.expr)
+        else:
+            print "%s = %s" % (self.variable, self.expr)
+
+class AxiomUnarySet:
+    def __init__(self, variables, expr):
+        self.axiom_set = []
+        for key in variables:
+            if key == expr.atomic[0]:
+                if expr.op:
+                    self.axiom_set.append(Axiom(key, "%s %s %s" % (expr.atomic[1:4])))
+                else:
+                    self.axiom_set.append(Axiom(key, expr.atomic[1]))
+            else:
+                self.axiom_set.append(Axiom(key, key))
+        self.condition = None
+
+    def set_condition(self, condition, flag):
+        if flag:
+            self.condition = "%s %s %s" % condition
+        else:
+            self.condition = "not (%s %s %s)" % condition
+        for i in xrange(0, len(self.axiom_set)):
+            self.axiom_set[i].set_condition(self.condition)
+
+    def phi_n(self, variables, natural):
+        for i in xrange(0, len(self.axiom_set)):
+            self.axiom_set[i].phi_n(variables, natural)
+
+    def to_loop(self, variables):
+        self.phi_n(variables, 'n')
+        for key in variables:
+            self.axiom_set.append(Axiom("%s(0)" % key, key, self.condition))
+            self.axiom_set.append(Axiom(key, "%s(N)" % key, self.condition))
+
+    def show(self):
+        print "axioms:"
+        for a in self.axiom_set:
+            a.show()
+
+# Smallest expression.
+class Smallest:
+    def __init__(self, condition):
+        self.e = 'N'
+        self.n = 'n'
+        self.condition = "%s %s %s" % condition
+
+    def show(self):
+        print "Smallest(%s, %s, not %s)" % (self.e, self.n, self.condition)
 
 def testAST():
     print "********Test AST********"
-    aenode1 = AENode('x', 'y', '+', 'z')
+    aenode1 = AtomicExprNode('x', 'y', '+', 'z')
     aenode1.show()
-    aenode2 = AENode('x', '4')
+    aenode2 = AtomicExprNode('x', '4')
     aenode2.show()
-    abnode = ABNode('x', '==', 'y')
+    abnode = AtomicBoolNode('x', '==', 'y')
     abnode.show()
-    pnode1 = PNode('unary', {'A': aenode1})
+    pnode1 = ProgNode('unary', {'A': aenode1})
     pnode1.show()
-    pnode2 = PNode('binary', {'P1': pnode1, 'P2': pnode1})
+    pnode2 = ProgNode('binary', {'P1': pnode1, 'P2': pnode1})
     pnode2.show()
-    pnode3 = PNode('judge', {'B': abnode, 'P1': pnode1, 'P2': pnode2})
+    pnode3 = ProgNode('judge', {'B': abnode, 'P1': pnode1, 'P2': pnode2})
     pnode3.show()
-    pnode4 = PNode('loop', {'B': abnode, 'P': pnode3})
+    pnode4 = ProgNode('loop', {'B': abnode, 'P': pnode3})
     pnode4.show()
     print "********Test AST********"
 
--- trafol.py
+++ trafol.py
@@ -1,19 +1,22 @@
 """
-trafol.py--tokenizer and parser.
+trafol.py -- added tokenizer and parser.
 Formalization of Programs in First-Order Logic with Discrete Linear Order.
 
-version: 0.0.1
+version: 0.0.2
 author: legendlee(legendlee1314@gmail.com)
 """
 
 import ply.lex as lex
-import ply.yacc as yacc
-
-from structures import ABNode
-from structures import AENode
-from structures import PNode
 
 # Tokenizer
+
+"""
+Tokenizing Process:
+- Input String: 'if x > 1 then x = 0'
+- Tokenizers:
+  -- IF V(x) MORE N(1) THEN V(x) ASSIGN N(0)
+  -- V for VARIABLE, N for NUMBER
+"""
 
 # List of reserved words.
 reserved = {
@@ -36,6 +39,8 @@ tokens = [
     'EQUALS',
     'NUMBER',
     'VARIABLE',
+    'LBRACKET',
+    'RBRACKET',
 ] + list(reserved.values())
 
 # A string containing ignored characters.
@@ -50,6 +55,8 @@ t_LESS       = r'<'
 t_MEQ        = r'>\='
 t_MORE       = r'>'
 t_EQUALS     = r'\=\='
+t_LBRACKET   = r'{'
+t_RBRACKET   = r'}'
 
 # A regular expression rules for actions.
 def t_NUMBER(t):
@@ -70,14 +77,30 @@ def t_error(t):
 lexer = lex.lex()
 
 # Parser
+
+import ply.yacc as yacc
+
+from structures import AtomicExpression
+from structures import AtomicBoolean
+from structures import ExprNode
+from structures import ProgNode
+
+# All variables occuring whole program.
+variables = {}
+
+# AST extra map(string, node) of node.
+anodes, bnodes, enodes, pnodes = {}, {}, {}, {}
+
 """
 Grammar rules:
 
 S'   : P
-P    : A
+P    : E
      | P P
-     | if B then P else P
-     | while B do P
+     | if B then { P } else { P }
+     | while B do { P }
+E    : A
+     | E E
 A    : R = R
      | R = R + R
      | R = R - R
@@ -90,35 +113,47 @@ R    : V
      | N
 """
 
-# added structures.
-abnodes, aenodes, pnodes = {}, {}, {}
-variables, output = {}, ''
-
-def p_pro_axi(p):
-    'pro : axi'
-    p[0] = p[1] + ';'
-    pnodes[p[0]] = PNode('unary', {'A': aenodes[p[1]]})
+def p_pro_expr(p):
+    'pro : expr'
+    p[0] = p[1]
+    pnodes[p[0]] = ProgNode('unary', {'E': enodes[p[1]]})
+    enodes[p[1]].set_pre_node(pnodes[p[0]])
 
 def p_pro_2pro(p):
     'pro : pro pro'
     p[0] = p[1] + p[2]
-    pnodes[p[0]] = PNode('binary', {'P1': pnodes[p[1]], 'P2': pnodes[p[2]]})
+    pnodes[p[0]] = ProgNode('binary', {'P1': pnodes[p[1]], 'P2': pnodes[p[2]]})
+    pnodes[p[1]].set_pre_node(pnodes[p[0]])
+    pnodes[p[2]].set_pre_node(pnodes[p[0]])
 
 def p_pro_judge(p):
-    'pro : IF bool THEN pro ELSE pro'
-    p[0] = 'if(' + p[2] + ')then{' + p[4] + '}else{' + p[6] + '}'
-    child = {'B': abnodes[p[2]], 'P1': pnodes[p[4]], 'P2': pnodes[p[6]]}
-    pnodes[p[0]] = PNode('judge', child)
+    'pro : IF bool THEN LBRACKET pro RBRACKET ELSE LBRACKET pro RBRACKET'
+    p[0] = 'if(' + p[2] + ')then{' + p[5] + '}else{' + p[9] + '}'
+    next_node = {'B': bnodes[p[2]], 'P1': pnodes[p[5]], 'P2': pnodes[p[9]]}
+    pnodes[p[0]] = ProgNode('judge', next_node)
+    pnodes[p[5]].set_pre_node(pnodes[p[0]])
+    pnodes[p[9]].set_pre_node(pnodes[p[0]])
 
 def p_pro_loop(p):
-    'pro : WHILE bool DO pro'
-    p[0] = 'while(' + p[2] + '){' + p[4] + '}'
-    pnodes[p[0]] = PNode('loop', {'B': abnodes[p[2]], 'P': pnodes[p[4]]})
+    'pro : WHILE bool DO LBRACKET pro RBRACKET'
+    p[0] = 'while(' + p[2] + '){' + p[5] + '}'
+    pnodes[p[0]] = ProgNode('loop', {'B': bnodes[p[2]], 'P': pnodes[p[5]]})
+    pnodes[p[5]].set_pre_node(pnodes[p[0]])
 
+def p_expr_axi(p):
+    'expr : axi'
+    p[0] = p[1] + ';'
+    enodes[p[0]] = ExprNode('unary', {'A': anodes[p[1]]})
+
+def p_expr_2expr(p):
+    'expr : expr expr'
+    p[0] = p[1] + p[2]
+    enodes[p[0]] = ExprNode('binary', {'E1': enodes[p[1]], 'E2': enodes[p[2]]})
+     
 def p_axi_assign1(p):
     'axi : rep ASSIGN rep'
     p[0] = p[1] + '=' + p[3]
-    aenodes[p[0]] = AENode(p[1], p[3], None, None)
+    anodes[p[0]] = AtomicExpression(p[1], p[3], None, None)
 
 def p_axi_assign2(p):
     '''axi : rep ASSIGN rep PLUS rep
@@ -127,7 +162,7 @@ def p_axi_assign2(p):
         p[0] = p[1] + '=' + p[3] + '+' + p[5]
     elif p[4] == '-':
         p[0] = p[1] + '=' + p[3] + '-' + p[5]
-    aenodes[p[0]] = AENode(p[1], p[3], p[4], p[5])
+    anodes[p[0]] = AtomicExpression(p[1], p[3], p[4], p[5])
 
 def p_bool(p):
     '''bool : rep LEQ rep
@@ -145,11 +180,12 @@ def p_bool(p):
         p[0] = p[1] + '>' + p[3]
     elif p[2] == '>=':
         p[0] = p[1] + '>=' + p[3]
-    abnodes[p[0]] = ABNode(p[1], p[2], p[3])
+    bnodes[p[0]] = AtomicBoolean(p[1], p[2], p[3])
 
 def p_rep_v(p):
     'rep : VARIABLE'
     p[0] = p[1]
+    # Note: store variables map.
     variables[p[1]] = p[1]
 
 def p_rep_n(p):
@@ -163,35 +199,109 @@ def p_error(p):
 # Build the parser.
 parser = yacc.yacc()
 
-# Main recursion logic of translation.
-def translate(node, v):
-    global output
-    if isinstance(node, PNode):
-        if node.form == 'unary':
-            translate(node.child['A'], v)
-        elif node.form == 'binary':
-            translate(node.child['P1'], v)
-            translate(node.child['P2'], v)
-        elif node.form == 'judge':
-            node.child['B'].show()
-            output += '%s %s %s :\n' % node.child['B'].axiom
-            translate(node.child['P1'], v)
-            output += '^ %s %s %s :\n' % node.child['B'].axiom
-            translate(node.child['P2'], v)
-        elif node.form == 'loop':
-            node.child['B'].show()
-            translate(node.child['P'], v)
-    elif isinstance(node, AENode):
-        data = ''
-        for key in v:
-            if key == node.axiom[0]:
-                if node.op:
-                    data += "%s* = %s %s %s;\n" % node.axiom
-                else:
-                    data += "%s* = %s;\n" % node.axiom
-            else:
-                data += "%s* = %s;\n" % (key, key)
-        output += data
+# Translation Process
+
+from structures import MidExprList
+from structures import MidJudgeNode
+from structures import MidLoopNode
+from structures import AxiomUnarySet
+from structures import Smallest
+
+# Scan atomic expressions.
+def scan_for_exprs(node, mid_exprs_list):
+    if node.form == 'unary':
+        mid_exprs_list.add_expr(node.next_node['A'])
+    else:
+        scan_for_exprs(node.next_node['E1'], mid_exprs_list)
+        scan_for_exprs(node.next_node['E2'], mid_exprs_list)
+
+# Scan all nodes of AST and build the middle layer.
+def scan_nodes(node):
+    if node.form == 'unary':
+        mid_node = MidExprList()
+        scan_for_exprs(node.next_node['E'], mid_node)
+        return mid_node
+    elif node.form == 'judge':
+        mid_node = MidJudgeNode()
+        mid_node.add_condition(node.next_node['B'].atomic)
+        mid_node.set_childs(scan_nodes(node.next_node['P1']), scan_nodes(node.next_node['P2']))
+        return mid_node
+    elif node.form == 'loop':
+        mid_node = MidLoopNode()
+        mid_node.add_condition(node.next_node['B'].atomic)
+        mid_node.set_child(scan_nodes(node.next_node['P']))
+        return mid_node
+    elif node.form == 'binary':
+        mid_node = scan_nodes(node.next_node['P1'])
+        mid_node.set_next_node(scan_nodes(node.next_node['P2']))
+        return mid_node
+
+# Show all mid structures.
+def show_mid(mid):
+    if mid is not None:
+        mid.show()
+    if isinstance(mid, MidExprList):
+        print "->"
+        show_mid(mid.next_node)
+    elif isinstance(mid, MidJudgeNode):
+        print "1>"
+        show_mid(mid.first_child)
+        print "2>"
+        show_mid(mid.second_child)
+        print "->"
+        show_mid(mid.next_node)
+    elif isinstance(mid, MidLoopNode):
+        print "|>"
+        show_mid(mid.child)
+        print "->"
+        show_mid(mid.next_node)
+
+# Parse all mid expression list.
+def parse_mid_expr_list(mid_exprs_list):
+    axiom_set = []
+    for expr in mid_exprs_list:
+        axiom_set.append(AxiomUnarySet(variables, expr))
+    return axiom_set
+
+# Parse mid judge node.
+def parse_mid_judge(axiom_set, condition, flag):
+    for i in xrange(0, len(axiom_set)):
+        if isinstance(axiom_set[i], Smallest):
+            continue
+        axiom_set[i].set_condition(condition, flag)
+
+# Parse mid loop node.
+def parse_mid_loop(axiom_set):
+    for i in xrange(0, len(axiom_set)):
+        if isinstance(axiom_set[i], Smallest):
+            continue
+        axiom_set[i].to_loop(variables)
+
+# Parse mid nodes in middle layer.
+def transalte_mid(mid):
+    if mid is None:
+        return []
+    if isinstance(mid, MidExprList):
+        axiom_set = parse_mid_expr_list(mid.exprs)
+        return axiom_set + transalte_mid(mid.next_node)
+    elif isinstance(mid, MidJudgeNode):
+        axiom_set_1 = transalte_mid(mid.first_child)
+        parse_mid_judge(axiom_set_1, mid.condition, True)
+        axiom_set_2 = transalte_mid(mid.second_child)
+        parse_mid_judge(axiom_set_2, mid.condition, False)
+        return axiom_set_1 + axiom_set_2 + transalte_mid(mid.next_node)
+    elif isinstance(mid, MidLoopNode):
+        axiom_set = transalte_mid(mid.child)
+        parse_mid_loop(axiom_set)
+        axiom_set.append(Smallest(mid.condition))
+        return axiom_set + transalte_mid(mid.next_node)
+
+# Translation with scanning the AST.
+def translate(root):
+    mid_start = scan_nodes(root)
+    axiom_set = transalte_mid(mid_start)
+    for a in axiom_set:
+        a.show()
 
 # Test the lexer.
 def test_lexer(lexer, num, data):
@@ -207,9 +317,18 @@ def test_lex(lexer):
     print "********Test Lexer********"
     test_lexer(lexer=lexer, num=1, data="x = x + y y = x + 4")
     test_lexer(lexer=lexer, num=2, data="z <= y x > z")
-    test_lexer(lexer=lexer, num=3, data="if x == y then x = y else y = x")
-    test_lexer(lexer=lexer, num=4, data="while x >= 100 do x = x - 1")
+    test_lexer(lexer=lexer, num=3, data="if x == y then { x = y } else { y = x }")
+    test_lexer(lexer=lexer, num=4, data="while x >= 100 do { x = x - 1 }")
     print "********Test Lexer********"
+
+def test_yacc(parser):
+    print "********Test Parser********"
+    test_parser(parser=parser, num=1, data="x = x + 4")
+    test_parser(parser=parser, num=2, data="x = x + y y = x + 4 z = 2 y = 3")
+    test_parser(parser=parser, num=3, data="if x == y then { x = y } else { y = x z = 0}")
+    test_parser(parser=parser, num=4, data="while x >= 100 do { x = x - 1 }")
+    test_parser(parser=parser, num=5, data="x = 0 y = 0 while x < 4 do { if x == 2 then { y = 1 x = x + 1 } else { y = 0 } y = y + 1} x = y")
+    print "********Test Parser********"
 
 # Test the parser.
 def test_parser(parser, num, data):
@@ -219,26 +338,18 @@ def test_parser(parser, num, data):
     print "Result: %s" % result
     pnodes[result].show()
 
-def test_yacc(parser):
-    print "********Test Parser********"
-    test_parser(parser=parser, num=1, data="x = x + 4")
-    test_parser(parser=parser, num=2, data="x = y - 4")
-    test_parser(parser=parser, num=3, data="x = y")
-    test_parser(parser=parser, num=4, data="x = x + y y = x + 4 z = 2 y = 3")
-    test_parser(parser=parser, num=5, data="if x == y then x = y else y = x")
-    test_parser(parser=parser, num=6, data="while x >= 100 do x = x - 1")
-    print "********Test Parser********"
-
 def test(data):
     result = parser.parse(data)
-    translate(pnodes[result], variables)
+    translate(pnodes[result])
 
 if __name__ == '__main__':
 #test_lex(lexer)
 #test_yacc(parser)
-#test("x = y + 4")
-#test("x = 4 y = 2 z = x + y")
-#test("if x == y then x = y else y = x")
-#test("while x < 4 do x = x + 1")
-    test("while x < 4 do if x == 2 then y = 1 x = x + 1 else y = 0 x = x - 1")
-    print output
+#test("x = 1 y = x + 1")
+#test("y = y x = 1 x = x + 1")
+#test("if x == y then { x = 1 } else { y = x + 1 }")
+#test("x = 1 y = 2 while x < 4 do { x = x + 1 }")
+#test("while x < 5 do { if 5 > 1 then { x = x + 1 } else { x = x - 1}}")
+    test("while x < 4 do { while x > 1 do { x = x + 1 } }")
+#test("x = 0 y = 0 while x < 4 do { if x == 2 then { y = 1 x = x + 1 } else { y = 0 } y = y + 1} x = y")
+#test("i = 0 j = 9 while i < 10 do { i = i + 1 j = j - 1}")
